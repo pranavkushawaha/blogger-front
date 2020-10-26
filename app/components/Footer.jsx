b207f890e6dbf6fbc7bf817e30372f7b6e8b7028
pranavkushawaha <58804493+pranavkushawaha@users.noreply.github.com>
2020-10-26 10:12:56 +0530

    Updated names

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
     return (
     <footer className="border-top text-center small text-muted py-3">
       <p><Link to="/" className="mx-1">Home</Link> | <Link className="mx-1" to="/about-us">About Us</Link> | <Link className="mx-1" to="/terms">Terms</Link></p>
-      <p className="m-0">Copyright &copy; 2020 <Link to="/" className="text-muted">ComplexApp</Link>. Devepoled by Kushawaha Brothers | All rights reserved.</p>
+      <p className="m-0">Copyright &copy; 2020 <Link to="/" className="text-muted">BloggerApp</Link>. Devepoled by Mini Project Group 10 of BIET | All rights reserved.</p>
     </footer>
     );
 }
