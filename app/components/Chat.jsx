4b63aac4dea7938fe191dba1dc477a641f96c628
pranavkushawaha <58804493+pranavkushawaha@users.noreply.github.com>
2022-08-15 16:09:39 +0530

    Update Chat.jsx

--- app/components/Chat.jsx
+++ app/components/Chat.jsx
@@ -73,7 +73,7 @@ function Chat() {
   return (
     <div id="chat-wrapper" className={"chat-wrapper shadow border-top border-left border-right " + (appState.isChatOpen ? "chat-wrapper--is-visible" : "")}>
     <div className="chat-title-bar bg-primary">
-      Chat
+      Global Chat
       <span onClick={()=> appDispatch({type:'toggleChat'})} className="chat-title-bar-close">
         <i className="fas fa-times-circle"></i>
       </span>
