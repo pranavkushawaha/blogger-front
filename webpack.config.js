10170ad994e26ccb2fd549d971ae0f9fc379f523
pranavkushawaha <58804493+pranavkushawaha@users.noreply.github.com>
2023-01-18 22:28:18 +0530

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ config = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-react", ["@babel/preset-env", { targets: { node: "18" } }]],
+            presets: ["@babel/preset-react", ["@babel/preset-env", { targets: "defaults" }]],
           },
         },
       },
